bca20fd24ffd2d166bf71ba2b1662b4eff28bb3f
GustavoSVasconcelos <gustavo.vasconcelos02@outlook.com>
2025-05-06 14:28:22 -0400

    chore: remove arquivo não utilzado

--- src/utils/prismaClient.ts
+++
@@ -1,8 +0,0 @@
-// Este arquivo centraliza a criação e exportação do Prisma Client.
-// Ele permite que toda a aplicação utilize a mesma instância do Prisma para acessar o banco de dados.
-
-import { PrismaClient } from '../generated/prisma';
-
-const prisma = new PrismaClient();
-
-export default prisma;

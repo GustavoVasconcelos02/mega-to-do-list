e88e5c8b40ea37f9e6613082a7785d95b98a83c7
BrunoFHernandez <brunofelipehernandez@gmail.com>
2025-05-10 18:43:32 -0400

    refactor: melhora a estrutura do user.controller.ts

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,18 +1,14 @@
 import { Request, Response } from 'express';
-import prisma from '../utils/prisma_client';
-
+import { createUserDAO } from '../DAO/user.dao';
 
 export const createUser = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
 
   try {
-    const newUser = await prisma.users.create({
-      data: { name, email, password },
-    });
-
+    const newUser = await createUserDAO(name, email, password);
     res.status(201).json(newUser);
   } catch (error: any) {
     console.error('Erro ao criar usuário:', error);
     res.status(400).json({ error: 'Erro ao criar usuário. Verifique os dados e tente novamente.' });
   }
-};
+}

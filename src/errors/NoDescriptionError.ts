7760443ccc19c8bee97b7b4b666ee04829a2bfb6
BrunoFHernandez <brunofelipehernandez@gmail.com>
2025-05-13 15:32:43 -0400

    refactor: aprimora a estrutura do arquivo NoDescriptionError.ts

--- src/errors/NoDescriptionError.ts
+++ src/errors/NoDescriptionError.ts
@@ -1,6 +1,6 @@
 import { CustomError } from "./CustomError";
 
-export class NoDesriptionError extends CustomError{
+export class NoDescriptionError extends CustomError{
     constructor(message = "Sua tarefa precisa ter uma descrição!") {
         super(message, 400);
     }

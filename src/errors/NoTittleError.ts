808ef4d03d9cec215f2f448828cda56670077838
BrunoFHernandez <brunofelipehernandez@gmail.com>
2025-05-13 15:40:00 -0400

    refactor: remove a pasta NoTittleError.ts

--- src/errors/NoTittleError.ts
+++
@@ -1,7 +0,0 @@
-import { CustomError } from "./CustomError";
-
-export class NoTittleError extends CustomError{
-    constructor(message = "Sua tarefa precisa ter título!") {
-        super(message, 400);
-    }
-} 

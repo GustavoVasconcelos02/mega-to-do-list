a69df5f8ed4f95fdfdf64f734176b84d78c78132
BrunoFHernandez <brunofelipehernandez@gmail.com>
2025-05-17 16:11:08 -0400

    feat: adiciona validacao zod ao todo.controller.ts

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,63 +1,109 @@
 //lida com req e res http
-
 import { Request, Response } from 'express';
 import { taskService } from '../services/todo.services';
-import { CreateTaskDTO } from '../models/task_model';
+import { z } from 'zod';
+
+
+export const createTaskSchema = z.object({
+  title: z.string().min(3, { message: 'Titulo deve ter pelo menos 3 caracteres'}),
+  description: z.string().min(10, { message: "Descrição deve ter pelo menos 10 caracteres"}).optional(),
+  date: z.date().optional(),
+  priority: z.enum (["baixa", "media", "alta"]).optional(),
+  completed: z.boolean().optional(),
+  user_id: z.string().uuid({ message: "ID de usuario invalido"})
+});
 
 export const todoController = {
 
-  // validacao de dados e lida com response e request http da criacao de tarefas
-  async create(req: Request, res: Response): Promise<void> {
+  // validacao da criacao de tarefas
+  async createTask(req: Request, res: Response): Promise<any> {
+    // valida o corpo da requisição (req.body) com base no esquema definido em createTaskSchema
+    const result = createTaskSchema.safeParse(req.body);
+
+    if (!result.success) {
+      return res.status(400).json({
+        error: 'Dados inválidos',
+        details: result.error.errors,
+      });
+    }
+
     try {
-      const taskData: CreateTaskDTO = req.body;
+      const taskData = result.data;
       const newTask = await taskService.createTask(taskData);
       res.status(201).json(newTask);
     } catch (error: any) {
-      res.status(error.statusCode || 500).json({ error: error.message });
+      res.status(500).json({ error: error.message });
     }
   },
 
-    // validacao de dados e lida com response e request http da procura de tarefas por id
-  async getById(req: Request, res: Response): Promise<void> {
-    try {
-      const taskId = req.params.id;
-      const task = await taskService.getTaskById(taskId);
-      res.status(200).json(task);
-    } catch (error: any) {
-      res.status(error.statusCode || 500).json({ error: error.message });
-    }
-  },
+  //validacao da atualizacao de tarefas
+  async updateTask(req: Request, res: Response): Promise<any> {
+    const result = createTaskSchema.safeParse(req.body);
 
-    // validacao de dados e lida com response e request http da atualizacao de tarefas
-  async update(req: Request, res: Response): Promise<void> {
+    if (!result.success) {
+      return res.status(400).json({
+        error: 'Dados inválidos',
+        details: result.error.errors,
+      });
+    }
+
     try {
       const taskId = req.params.id;
-      const taskData: Partial<CreateTaskDTO> = req.body;
+      const taskData = result.data;
       const updatedTask = await taskService.updateTask(taskId, taskData);
       res.status(200).json(updatedTask);
     } catch (error: any) {
-      res.status(error.statusCode || 500).json({ error: error.message });
+      res.status(500).json({ error: error.message });
     }
   },
+  //valida se o id do usuario existe e deleta a tarefa existente no banco de dados 
+  async deleteTask(req: Request, res: Response): Promise<any> {
+    const taskId = req.params.id;
 
-    // validacao de dados e lida com response e request http da exclusao de tarefas 
-  async delete(req: Request, res: Response): Promise<void> {
+    if (!taskId) {
+      return res.status(400).json({ error: 'ID da tarefa é obrigatório' });
+    }
+
     try {
-      const taskId = req.params.id;
       const deletedTask = await taskService.deleteTask(taskId);
-      res.status(200).json(deletedTask);
+      if (!deletedTask) {
+        return res.status(404).json({ error: 'Tarefa não encontrada' });
+      }
+      res.status(204).send();
     } catch (error: any) {
-      res.status(error.statusCode || 500).json({ error: error.message });
+      res.status(500).json({ error: error.message });
     }
   },
 
-    // lida com response e request http da listagem de tarefas 
-  async getAll(req: Request, res: Response): Promise<void> {
+  //valida o id informado e procura a tarfa referente esse id no banco de dados
+  async getTaskById(req: Request, res: Response): Promise<any> {
+    const taskId = req.params.id;
+
+    if (!taskId) {
+      return res.status(400).json({ error: 'ID da tarefa é obrigatório' });
+    }
+
+    try {
+      const task = await taskService.getTaskById(taskId);
+      if (!task) {
+        return res.status(404).json({ error: 'Tarefa não encontrada' });
+      }
+      res.status(200).json(task);
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  //lista todas as tarefas criadas no banco de dados 
+  async getAllTask(req: Request, res: Response): Promise<void> {
     try {
       const tasks = await taskService.getAllTasks();
       res.status(200).json(tasks);
     } catch (error: any) {
-      res.status(error.statusCode || 500).json({ error: error.message });
+      res.status(500).json({ error: error.message });
     }
   },
 };
+
+
+
+

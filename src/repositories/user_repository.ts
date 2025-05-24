8e1c334858dcf1d9c7ae0643cc0f2c17ddcf2a75
BrunoFHernandez <brunofelipehernandez@gmail.com>
2025-05-24 17:14:35 -0400

    feat: adiciona proteção de senha no cadastro

--- src/repositories/user_repository.ts
+++ src/repositories/user_repository.ts
@@ -1,7 +1,13 @@
-import prisma from '../utils/prisma_client'
+import prisma from '../utils/prisma_client';
+import bcrypt from 'bcryptjs';
 
 export const createUserDAO = async (name: string, email: string, password: string) => {
-    return await prisma.users.create({
-        data: {name, email, password}
-    })
-}; 
+  const hashedPassword = await bcrypt.hash(password, 10); // Criptografa a senha
+  return await prisma.users.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword
+    }
+  });
+};

66f35c1e8856213a1871a2465501321220f98fb6
BrunoFHernandez <brunofelipehernandez@gmail.com>
2025-05-24 17:15:25 -0400

    feat: adiciona rota de login no user_routes.ts

--- src/routes/user_routes.ts
+++ src/routes/user_routes.ts
@@ -1,9 +1,12 @@
-// src/routes/user.routes.ts
+// routes/user_routes.ts
 import { Router } from 'express';
-import { createUser } from '../controllers/user_controller';
+import { createUser} from '../controllers/user_controller';
+import { loginUser } from '../controllers/login_controller';
+
 
 const router = Router();
 
-router.post('/users', createUser);
+router.post('/users', createUser);        
+router.post('/login', loginUser);
 
 export default router;

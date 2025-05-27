67604282cb25c9a1ae26a7072a109efdee2400a6
BrunoFHernandez <brunofelipehernandez@gmail.com>
2025-05-27 15:11:31 -0400

    feat: adiciona updateUserDAO e deleteUserDAO

--- src/repositories/user_repository.ts
+++ src/repositories/user_repository.ts
@@ -1,6 +1,7 @@
 import prisma from '../utils/prisma_client';
 import bcrypt from 'bcryptjs';
 
+//cadastra um usuario 
 export const createUserDAO = async (name: string, email: string, password: string) => {
   const hashedPassword = await bcrypt.hash(password, 10); 
   return await prisma.users.create({
@@ -11,3 +12,26 @@ export const createUserDAO = async (name: string, email: string, password: strin
     }
   });
 };
+
+//atualiza um usuario
+export async function updateUserDAO(
+  id: string,
+  data: { name?: string; email?: string; password?: string }
+) {
+
+  if (data.password) {
+    data.password = await bcrypt.hash(data.password, 10);
+  }
+
+  return prisma.users.update({
+    where: { id },
+    data,
+  });
+}
+
+//exclui um usuario pelo ID
+export async function deleteUserDAO(id: string) {
+  return prisma.users.delete({
+    where: { id },
+  });
+}
